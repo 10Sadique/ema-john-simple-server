614130891e1baababfd2a9b5453693d9d6f28ecb
10Sadique <jafarsjahan@gmail.com>
2022-11-03 14:38:26 +0600

    mongoDB added

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 // middlewared
 app.use(cors());
 app.use(express.json());
 
+// mongoDB connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.onfc57d.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    try {
+        const productCollection = client.db('emaJohn').collection('products');
+    } finally {
+    }
+}
+
+run().catch((err) => console.dir);
+
+// init server
 app.get('/', (req, res) => {
     res.send({
         message: 'Ema-John Simple Server',
